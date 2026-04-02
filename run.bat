f7210e147407d4c1c74c799ba2bd045cf999cd55
MagomedMagomedov <105859497+MagomedovMagomedSh@users.noreply.github.com>
2026-04-02 05:28:49 +0000

    fix: run streamlit as py module

--- run.bat
+++ run.bat
@@ -1,4 +1,13 @@
 @echo off
-pip install -r requirements.txt
-streamlit run main.py
+echo [1/3] Checking dependencies...
+:: Установка библиотек из requirements.txt
+python -m pip install -r requirements.txt
+
+echo.
+echo [2/3] Starting Streamlit app...
+:: Запуск через модуль python -m, чтобы обойти ошибку с PATH
+python -m streamlit run main.py
+
+echo.
+echo [3/3] Application closed.
 pause
